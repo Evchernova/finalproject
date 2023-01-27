828cd81b67c58e245a1bbe7fd5e6a04a9942a43f
Евгения <ev.chernova@icloud.com>
2023-01-28 00:03:11 +0300

    task solution added

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма.*/
+
+
+void ShortArray(string[] array, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        newArray[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+string[] array = new string[4] {"hello", "2", "world", ":-)"};
+string[] newArray = new string[array.Length];
+ShortArray(array, newArray);
+PrintArray(newArray);
